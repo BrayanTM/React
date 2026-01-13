e21e58f816e0e61d86c15abd127997e8fd724e82
BrayanTM <brayantebelan@gmail.com>
2026-01-12 20:14:18 -0600

    Funciones con múltiples retornos

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,9 +2,7 @@ function greet(name: string): string {
   return `Hola ${name}`;
 }
 
-const greet2 = (name: string): string => {
-  return `Hola ${name}`;
-};
+const greet2 = (name: string): string => `Hola ${name}`;
 
 const message = greet("Goku");
 const message2 = greet2("Veggeta");
@@ -12,21 +10,34 @@ const message2 = greet2("Veggeta");
 console.log(message);
 console.log(message2);
 
-function getUser() {
+interface User {
+  uid: string;
+  userName: string;
+}
+
+function getUser(): User {
   return {
     uid: "ABC-123",
     userName: "El_Papi23",
   };
 }
 
-const getUser2 = () => {
-  return {
-    uid: "ABC-123",
-    userName: "El_Papi23",
-  };
-};
+const getUser2 = (): User => ({
+  uid: "ABC-321",
+  userName: "Manolito_Ferragamo",
+});
 
 const user = getUser();
 const user2 = getUser2();
 console.log(user);
 console.log(user2);
+
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+// myNumbers.forEach(function (value) {
+//   console.log({ value });
+// });
+
+// myNumbers.forEach((value) => console.log({ value }));
+
+myNumbers.forEach(console.log);
