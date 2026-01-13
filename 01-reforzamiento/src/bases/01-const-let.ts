3a2ebc7a02564a8611bd5117cf90cf50ad36e5ba
BrayanTM <brayantebelan@gmail.com>
2026-01-12 18:43:06 -0600

    Variables y Constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,8 @@
+const firstName: string = "Brayan";
+const lastName: string = "Tebelan";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterH = lastName.includes("n");
+console.log({ containsLetterH, diceNumber, firstName, lastName });
