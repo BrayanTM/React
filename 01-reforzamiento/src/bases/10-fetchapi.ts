4f852d20cd69232b8d402e5499dc3fb282ffb766
BrayanTM <brayantebelan@gmail.com>
2026-01-13 20:43:48 -0600

    Interfaces y Optimización

--- 01-reforzamiento/src/bases/10-fetchapi.ts
+++ 01-reforzamiento/src/bases/10-fetchapi.ts
@@ -1,19 +1,22 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const request = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
 );
 
+const createImageInsideDOM = (imageUrl: string) => {
+  const imageElement = document.createElement("img");
+  imageElement.src = imageUrl;
+  document.body.append(imageElement);
+};
+
 request
   .then((response) => response.json())
-  .then((data) => {
-    const imageUrl = data.data.images.original.url;
-    console.log(imageUrl);
-
-    const imageElement = document.createElement("img");
-    imageElement.src = imageUrl;
-
-    document.body.append(imageElement);
+  .then(({ data }: GiphyRandomResponse) => {
+    const imageUrl = data.images.original.url;
+    createImageInsideDOM(imageUrl);
   })
   .catch((error) => {
     console.error(error);
