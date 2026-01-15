87e348a22965776acc56e5121e9548f8d592f9d6
BrayanTM <brayantebelan@gmail.com>
2026-01-14 20:47:33 -0600

    Mostrar listados de elementos

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,13 +1,29 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: "Nintendo Switch 2", quantity: 1 },
+  { productName: "Logitech MX Masters 4", quantity: 5 },
+  { productName: "Intel Ultra 9", quantity: 3 },
+  { productName: "Silla Secret Lab", quantity: 8 },
+  { productName: "Play Station 6", quantity: 8 },
+];
+
 export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de Compras</h1>
-      <ItemCounter name="Nintendo Switch 2" quantity={4} />
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter key={productName} name={productName} quantity={quantity} />
+      ))}
+      {/* <ItemCounter name="Nintendo Switch 2" quantity={4} />
       <ItemCounter name="Logitech MX Masters 4" quantity={6} />
       <ItemCounter name="Intel Ultra 9" quantity={3} />
-      <ItemCounter name="Silla Secret Lab" quantity={8} />
+      <ItemCounter name="Silla Secret Lab" quantity={8} /> */}
     </>
   );
 }
