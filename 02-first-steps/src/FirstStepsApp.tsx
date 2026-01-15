2e95b401d9823ccb677d468417f334004760f66c
BrayanTM <brayantebelan@gmail.com>
2026-01-14 20:36:58 -0600

    Propiedades del componente - Props

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -4,9 +4,10 @@ export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de Compras</h1>
-      <ItemCounter />
-      <ItemCounter />
-      <ItemCounter />
+      <ItemCounter name="Nintendo Switch 2" quantity={4} />
+      <ItemCounter name="Logitech MX Masters 4" quantity={6} />
+      <ItemCounter name="Intel Ultra 9" quantity={3} />
+      <ItemCounter name="Silla Secret Lab" quantity={8} />
     </>
   );
 }
