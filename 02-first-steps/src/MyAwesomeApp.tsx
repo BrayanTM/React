e50ac43e3510fbf843e7d04bb352b08079f409a1
BrayanTM <brayantebelan@gmail.com>
2026-01-14 20:08:12 -0600

    Colocar estilos de CSS

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,13 +1,22 @@
+import type { CSSProperties } from "react";
+
 const firstName: string = "Brayan";
 const lastName: string = "Tebelan";
 
 const favoriteGames: string[] = ["Elder Ring", "Smash", "Halo"];
 
-const isActive: boolean = false;
+const isActive: boolean = true;
 
 const address = {
   zipCode: 14001,
   country: "Guatemala",
+};
+
+const myStyles: CSSProperties = {
+  backgroundColor: isActive ? "green" : "yellow",
+  borderRadius: 10,
+  padding: 10,
+  marginTop: 30,
 };
 
 export function MyAwesomeApp() {
@@ -21,7 +30,7 @@ export function MyAwesomeApp() {
 
       <h1>{isActive ? "Activo" : "Inactivo"}</h1>
 
-      <p>{JSON.stringify(address)}</p>
+      <p style={myStyles}>{JSON.stringify(address)}</p>
     </>
   );
 }
