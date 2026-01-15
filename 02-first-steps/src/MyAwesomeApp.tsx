10517a7e827dc5d0973d538188c3b99c35fd7d79
Ahau-x Dev <erivas@ahau-x.com>
2026-01-14 19:35:38 -0600

    Impresión de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
+const firstName: string = "Brayan";
+const lastName: string = "Tebelan";
+
+const favoriteGames: string[] = ["Elder Ring", "Smash", "Halo"];
+
+const isActive: boolean = false;
+
+const address = {
+  zipCode: 14001,
+  country: "Guatemala",
+};
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Jose</h1>
-      <h3>Tebelan</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(", ")}</p>
+      <p>{2 + 2}</p>
+
+      <h1>{isActive ? "Activo" : "Inactivo"}</h1>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   );
 }
