f2b43a1bc85780c7a9829ed3dde895e8d998627a
BrayanTM <brayantebelan@gmail.com>
2026-01-15 21:14:24 -0600

    Agrupar pruebas similares

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,14 +1,68 @@
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { describe, expect, test } from "vitest";
+import { add, multiply, subtract } from "./math.helper";
 
-test("should add two positeive numbers", () => {
-  // ! 1. Arrange
-  const a = 3;
-  const b = 3;
+describe("add", () => {
+  test("should add two positeive numbers", () => {
+    // ! 1. Arrange
+    const a = 3;
+    const b = 3;
 
-  // ! 2. Act
-  const result = add(a, b);
+    // ! 2. Act
+    const result = add(a, b);
 
-  // ! 3. Assert
-  expect(result).toBe(6);
+    // ! 3. Assert
+    expect(result).toBe(a + b);
+  });
+});
+
+describe("subtract", () => {
+  test("should subtract two positeive numbers", () => {
+    // ! 1. Arrange
+    const a = 3;
+    const b = 2;
+
+    // ! 2. Act
+    const result = subtract(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a - b);
+  });
+
+  test("should subtract two negative numbers", () => {
+    // ! 1. Arrange
+    const a = -3;
+    const b = -2;
+
+    // ! 2. Act
+    const result = subtract(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a - b);
+  });
+});
+
+describe("multiply", () => {
+  test("should multiply two positeive numbers", () => {
+    // ! 1. Arrange
+    const a = 3;
+    const b = 3;
+
+    // ! 2. Act
+    const result = multiply(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a * b);
+  });
+
+  test("should multiply two negative numbers", () => {
+    // ! 1. Arrange
+    const a = -3;
+    const b = -2;
+
+    // ! 2. Act
+    const result = multiply(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a * b);
+  });
 });
