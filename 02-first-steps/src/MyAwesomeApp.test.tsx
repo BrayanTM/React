f0a0ab3a85194404a2081273c56c0148321ef5e4
BrayanTM <brayantebelan@gmail.com>
2026-01-18 17:23:46 -0600

    Buscar elementos en el componente renderizado

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,11 +1,28 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
 describe("MyAwesomeApp", () => {
   test("should render firstName and lastName", () => {
-    render(<MyAwesomeApp />);
+    const { container } = render(<MyAwesomeApp />);
 
+    const h1 = container.querySelector("h1");
+    const h3 = container.querySelector("h3");
+
+    expect(h1?.innerHTML).toContain("Brayan");
+    expect(h3?.innerHTML).toContain("Tebelan");
+  });
+
+  test("should render firstName and lastName - Screen", () => {
+    render(<MyAwesomeApp />);
     screen.debug();
+
+    // const h1 = screen.getByRole("heading", {
+    //   level: 1,
+    // });
+
+    const h1 = screen.getByTestId("first-name-title");
+
+    expect(h1.innerHTML).toContain("Brayan");
   });
 });
