4e665c0cfbca36901c1f5bdb1494b51ac1d5c5e4
BrayanTM <brayantebelan@gmail.com>
2026-01-18 19:05:31 -0600

    Esperar argumentos específicos

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,14 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 
+const mockItemCounter = vi.fn((_props: unknown) => {
+  return <div data-testid="item-counter-mock" />;
+});
+
 vi.mock("./shopping-cart/ItemCounter", () => ({
-  ItemCounter: () => {
-    return <div data-testid="item-counter-mock">ItemCounter Mock</div>;
-  },
+  ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
+// vi.mock("./shopping-cart/ItemCounter", () => ({
+//   ItemCounter: (props: unknown) => {
+//     return (
+//       <div
+//         data-testid="item-counter-mock"
+//         name={props.name}
+//         quantity={props.quantity}
+//       >
+//         ItemCounter Mock
+//       </div>
+//     );
+//   },
+// }));
+
 describe("FirstStepsApp", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test("should match snapshot", () => {
     const { container } = render(<FirstStepsApp />);
 
@@ -23,5 +44,31 @@ describe("FirstStepsApp", () => {
     expect(itemCounters.length).toBe(5);
 
     screen.debug();
+  });
+
+  test("should render ItemCounter with correct props", () => {
+    render(<FirstStepsApp />);
+
+    expect(mockItemCounter).toHaveBeenCalledTimes(5);
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Nintendo Switch 2",
+      quantity: 1,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Logitech MX Masters 4",
+      quantity: 5,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Intel Ultra 9",
+      quantity: 3,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Silla Secret Lab",
+      quantity: 8,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: "Play Station 6",
+      quantity: 8,
+    });
   });
 });
