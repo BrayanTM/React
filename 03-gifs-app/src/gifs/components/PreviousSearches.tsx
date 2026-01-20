9c610dd2eb07515cca9aafefd24ac0147a83ced2
BrayanTM <brayantebelan@gmail.com>
2026-01-19 19:11:30 -0600

    Pensemos en componentes

---
+++ 03-gifs-app/src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,12 @@
+export const PreviousSearches = () => {
+  return (
+    <div className="previous-searches">
+      <h2>Busquedas Previas</h2>
+      <ul className="previous-searches-list">
+        <li>Goku</li>
+        <li>Vegetta</li>
+        <li>Gohan</li>
+      </ul>
+    </div>
+  );
+};
