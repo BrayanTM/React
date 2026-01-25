7f38195956341d5605a6231111c04eaff519ad12
BrayanTM <brayantebelan@gmail.com>
2026-01-24 19:31:06 -0600

    Componentes con "Custom Hooks"

---
+++ 03-gifs-app/src/counter/components/MyCounterApp.test.tsx
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "vitest";
+import { MyCounterApp } from "./MyCounterApp";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("MyCounterApp", () => {
+  test("should rendet the component", () => {
+    render(<MyCounterApp />);
+
+    screen.debug();
+
+    expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain(
+      "Counter: 1",
+    );
+
+    expect(screen.getByRole("button", { name: "+1" })).toBeDefined();
+    expect(screen.getByRole("button", { name: "-1" })).toBeDefined();
+    expect(screen.getByRole("button", { name: "Reset" })).toBeDefined();
+  });
+
+  test("should increment the counter", () => {
+    render(<MyCounterApp />);
+
+    const labelH1 = screen.getByRole("heading", { level: 1 });
+    const button = screen.getByRole("button", { name: "+1" });
+
+    fireEvent.click(button);
+
+    expect(labelH1.innerHTML).toContain("Counter: 2");
+  });
+
+  test("should decrement the counter", () => {
+    render(<MyCounterApp />);
+
+    const labelH1 = screen.getByRole("heading", { level: 1 });
+    const button = screen.getByRole("button", { name: "-1" });
+
+    fireEvent.click(button);
+
+    expect(labelH1.innerHTML).toContain("Counter: 0");
+  });
+});
