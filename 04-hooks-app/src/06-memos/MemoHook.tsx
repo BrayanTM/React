7f8b23238796185c97bfe154b4749f7b4ddbcf3b
Brayan Tebelan <brayantebelan@gmail.com>
2026-04-04 17:40:55 -0600

    useCallback - Memorización de funciones

--- 04-hooks-app/src/06-memos/MemoHook.tsx
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { MyTitle } from "./ui/MyTitle";
 import { MySubTitle } from "./ui/MySubtitle";
 
@@ -6,12 +6,16 @@ export const MemoHook = () => {
   const [title, setTitle] = useState("Hola");
   const [subTitle, setSubTitle] = useState("Mundo");
 
+  const handleMyAPICall = useCallback(() => {
+    console.log("Llamar a mi API - ", subTitle);
+  }, [subTitle]);
+
   return (
     <div className="bg-gradient flex flex-col gap-4">
       <h1 className="text-2xl font-thin text-white">Memo Hook</h1>
 
       <MyTitle title={title} />
-      <MySubTitle subTitle={subTitle} />
+      <MySubTitle subTitle={subTitle} callMyAPI={handleMyAPICall} />
 
       <button
         className="cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-white"
--- 04-hooks-app/src/06-memos/ui/MySubTitle.tsx
+++ 04-hooks-app/src/06-memos/ui/MySubTitle.tsx
@@ -2,15 +2,19 @@ import React from "react";
 
 interface Props {
   subTitle: string;
+  callMyAPI: () => void;
 }
 
-export const MySubTitle = React.memo(({ subTitle }: Props) => {
+export const MySubTitle = React.memo(({ subTitle, callMyAPI }: Props) => {
   console.log("MySubTitle re-render");
 
   return (
     <>
       <h6 className="text-2xl font-bold">{subTitle}</h6>
-      <button className="cursor-pointer rounded-md bg-indigo-500 px-2 py-1 text-white">
+      <button
+        className="cursor-pointer rounded-md bg-indigo-500 px-2 py-1 text-white"
+        onClick={callMyAPI}
+      >
         Lammar a Funcion
       </button>
     </>
