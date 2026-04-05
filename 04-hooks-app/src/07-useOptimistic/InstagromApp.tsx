45510a24cd62402c806faff7078577500fdaac36
Brayan Tebelan <brayantebelan@gmail.com>
2026-04-04 18:39:18 -0600

    useOptimistic - Preparación del ejercicio

---
+++ 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+interface Comment {
+  id: number;
+  text: string;
+  optimistic?: boolean;
+}
+
+export const InstagromApp = () => {
+  const [comments, setComments] = useState<Comment[]>([
+    { id: 1, text: "¡Gran foto!" },
+    { id: 2, text: "Me encanta 🧡" },
+  ]);
+
+  const handleAddComment = async (formData: FormData) => {
+    const messageText = formData.get("post-message") as string;
+    console.log("Nuevo comentario", messageText);
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    setComments((prev) => [
+      ...prev,
+      {
+        id: new Date().getTime(),
+        text: messageText,
+      },
+    ]);
+  };
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-slate-700">
+      {/* Post de ejemplo */}
+      <div className="flex w-125 flex-col items-center justify-center rounded-t-3xl bg-gray-300 p-4">
+        <img
+          src="https://images.unsplash.com/photo-1649972904349-6e44c42644a7?w=500&h=500&fit=crop"
+          alt="Instagrom"
+          className="mb-4 rounded-xl object-cover"
+        />
+        <p className="mb-4 font-bold text-black">
+          Mira que interesante esta funcionalidad de la API de React.
+        </p>
+      </div>
+
+      {/* Comentarios */}
+      <ul className="flex w-125 flex-col items-start justify-center bg-gray-300 p-4">
+        {comments.map((comment) => (
+          <li key={comment.id} className="mb-2 flex items-center gap-2">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-500">
+              <span className="text-center text-white">A</span>
+            </div>
+            <p className="text-black">{comment.text}</p>
+            {comment.optimistic && (
+              <span className="text-sm text-gray-500">enviando... </span>
+            )}
+          </li>
+        ))}
+      </ul>
+
+      {/* Formulario de comentarios */}
+      <form
+        action={handleAddComment}
+        className="flex w-125 flex-col items-center justify-center rounded-b-3xl bg-gray-300 p-4"
+      >
+        <input
+          type="text"
+          name="post-message"
+          placeholder="Escribe un comentario"
+          required
+          className="mb-2 w-full rounded-md bg-white p-2 text-black"
+        />
+        <button
+          type="submit"
+          disabled={false}
+          className="w-full rounded-md bg-blue-500 p-2 text-white"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
